c1295fba5db72020977806231d39e82471070566
Brian Anderson <andersrb@gmail.com>
2010-03-13 17:58:21 -0500

    Accept port number as a command line parameter

--- instant-httpd.py
+++ instant-httpd.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 
 import socket
+import sys
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 def main():
     try:
         server = None
-        for port in range(8080, 8180):
-            try:
-                server = HTTPServer(('', port), SimpleHTTPRequestHandler)
-            except socket.error:
-                continue
-            print('Listening on port {0}'.format(port))
-            break
+
+        port = 0
+
+        if (len(sys.argv) == 2):
+            port = int(sys.argv[1])
+            server = HTTPServer(('', port), SimpleHTTPRequestHandler)
+        else:
+            for port in range(8080, 8180):
+                try:
+                    server = HTTPServer(('', port), SimpleHTTPRequestHandler)
+                except socket.error:
+                    continue
+                break
+
+        print('Listening on port {0}'.format(port))
 
         server.serve_forever()
     except KeyboardInterrupt:
